ac71fd4d91c0c5add61b0e1a11ffcc155741c4ca
Aviprit Shah <avipritshah15@gmail.com>
2023-06-29 15:40:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import play from "./play.png";
 
 function App() {
+  const [text, setText] = useState("");
+  const speech = new SpeechSynthesisUtterance();
+  speech.voice = speechSynthesis.getVoices()[156];
+  speech.lang = "hi-IN";
+
+  const fetchData = async () => {
+    let data = new URLSearchParams();
+    data.append("text", text);
+
+    console.log(data);
+    const res = await fetch("http://localhost:8000/translate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: data,
+    });
+    return res.json();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`form submitted: ${text}`);
+    fetchData().then((data) => {
+      speech.text = data.result;
+    });
+  };
+
+  const handleClick = () => {
+    window.speechSynthesis.speak(speech);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-container">
+        <form className="form" onSubmit={handleSubmit}>
+          <p className="head_text">Input your text here</p>
+          <label>
+            <div className="label_text">Text Input</div>
+            <input
+              type="text"
+              placeholder=""
+              title=""
+              className="input"
+              onChange={(e) => setText(e.target.value)}
+            />
+          </label>
+          <button type="submit" className="button">
+            Translate to Hindi
+          </button>
+          <button type="button" className="play" onClick={handleClick}>
+            <img src={play} alt="" />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
